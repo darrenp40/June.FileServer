eca60b6a88b854940cfaf0cecaa9425b72881cdb
darrenp40 <darrenp40@gmail.com>
2023-06-18 19:37:53 +0100

    Finished Creator Routes

--- models/creator.js
+++ models/creator.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Document = require('./document')
 
 const creatorSchema = new mongoose.Schema({
 name: {
@@ -6,5 +7,51 @@ name: {
     required: true 
 }
 })
+
+
+creatorSchema.pre("deleteOne", async function (next) {
+    try {
+        const query = this.getFilter();
+        const hasDocument = await Document.exists({ creator: query._id });
+  
+        if (hasDocument) {
+            next(new Error("This creator still has documents."));
+        } else {
+            next();
+        }
+    } catch (err) {
+        next(err);
+    }
+});
+
+
+/*creatorSchema.pre('findByIdAndRemove', async function () {
+    const documents = await Document.find({ creator: this.id });
+    if (documents.length > 0) {
+        throw new Error('This creator still has documents');
+    }
+})*/
+
+
+/*creatorSchema.pre('findByIdAndRemove', async function() {
+    const documents = await Document.find({ creator: this.id }).exec();
+
+    if (documents.length > 0) {
+        throw new Error('This creator still has documents');
+    }
+});*/
+
+
+/*creatorSchema.pre('remove', function(next) {
+    Document.find({ creator: this.id }, (err, documents) => {
+        if (err) {
+            next(err)
+        } else if (documents.length > 0) {
+            next(new Error('This creator still has documents'))
+        } else {
+            next()
+        }
+    })
+})*/
 
 module.exports = mongoose.model('Creator', creatorSchema)
--- routes/creators.js
+++ routes/creators.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Creator = require('../models/creator')
+const Document = require('../models/document')
 
 //All Creators Route
 
@@ -12,24 +13,14 @@ router.get('/', async (req, res) => {
     try {
         const creators = await Creator.find(searchOptions);
         res.render('creators/index', { 
-            creators, 
+            creators, creators,
             searchOptions: req.query 
-        });
-    } catch (error) {
-        console.error(error);
-        res.redirect('/');
-    }
-});
-
-/*router.get('/', async (req, res) => {
-    try {
-        const creators = await Creator.find({})
-        res.render('creators/index', { creators: creators })
+        })
     } catch {
-        res.redirect('/')
+       res.redirect('/');
     }
-    res.render('creators/index')
-})*/
+})
+
 
 //New Creator Route
 router.get('/new', (req, res) => {
@@ -40,31 +31,116 @@ router.get('/new', (req, res) => {
 
 router.post('/', async (req, res) => {
     const creator = new Creator({
-        name: req.body.name
+      name: req.body.name
     })
-        
-    const newCreator = await creator.save()
-    .then((newCreator) => {
-        res.redirect(`creators`);
-    })
-    .catch((err) => {
-        res.render('creators/new', {
-            creator: creator,
-            errorMessage: 'Error creating Creator'
-        });
-    })
-         
-    /*creator.save((err, newCreator) => {
-        if (err) {
-            res.render('creators/new', {
-                creator: creator,
-                errorMessage: 'Error creating Creator'
-            })
-        } else {
-            //res.redirect(`creators/${newCreator.id}`)
-            res.redirect(`creators`)
-        }*/
+    try {
+      const newCreator = await creator.save()
+      res.redirect(`creators/${newCreator.id}`)
+    } catch {
+      res.render('creators/new', {
+        creator: creator,
+        errorMessage: 'Error creating Creator'
+      })
+    }
+  })
+// Show Creator Route
 
+  router.get('/:id', async (req, res) => {
+    try {
+      const creator = await Creator.findById(req.params.id)
+      const documents = await Document.find({ creator: creator.id }).limit(6).exec()
+      res.render('creators/show', {
+        creator: creator,
+        documentsByCreator: documents
+      })
+    } catch {
+      res.redirect('/')
+    }
+  })
+  
+  // Edit Creator Route
+
+  router.get('/:id/edit', async (req, res) => {
+    try {
+      const creator = await Creator.findById(req.params.id)
+      res.render('creators/edit', { creator: creator })
+    } catch {
+      res.redirect('/creators')
+    }
+  })
+ 
+  // Update Creator Route
+
+    /*router.put('/:id', async (req, res) => {
+        let creator
+        try {
+          creator = await Creator.findById(req.params.id)
+          creator.name = req.body.name
+          await creator.save()
+          res.redirect(`/creators/${author.id}`)
+        } catch {
+          if (creator == null) {
+            res.redirect('/')
+          } else {
+            res.render('creators/edit', {
+              creator: creator,
+              errorMessage: 'Error updating Creator'
+            })
+          }*/
+          router.put('/:id', async (req, res) => {
+            try {
+              const creator = await Creator.findById(req.params.id);
+              if (!creator) {
+                return res.redirect('/');
+              }
+          
+              creator.name = req.body.name;
+              await creator.save();
+          
+              res.redirect(`/creators/${creator.id}`);
+            } catch (error) {
+              res.render('creators/edit', {
+                creator: creator,
+                errorMessage: 'Error updating Creator',
+              });
+            }
+          });
+          
+      
+      
+    
+      // Delete Creator Route
+
+      router.delete('/:id', async (req, res) => {
+       let creator
+        try {
+          //const creator = await Creator.findByIdAndRemove(req.params.id);
+          const response = await Creator.deleteOne({_id: req.params.id})
+          creator = await Creator.findById(req.params.id)
+          await creator.remove()
+          res.redirect('/creators')
+
+          //router.delete('/:id', async (req, res) => {
+          //let creator
+          //  try {
+          //    const creator = await Creator.findByIdAndRemove(req.params.id);
+             // if (!creator) {
+             //   return res.status(404).json({ error: 'Creator not found' });
+             // }
+            //  res.redirect('/creators');
+            //} catch (error) {
+            //  res.status(500).json({ error: 'Internal server error' });
+            //}
+          //});
+              
+        } catch {
+          if (creator == null) {
+            res.redirect('/')
+          } else {
+            res.redirect(`/creators/${creator.id}`)
+          }
+        }
+    
     })
   
 
