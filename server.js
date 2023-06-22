1435372c7218915875abfe1829e5e85e380dc70e
darrenp40 <darrenp40@gmail.com>
2023-06-22 11:56:37 +0100

    Added Green Gen Styles

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-//if (process.env !== 'production'){
-//require('dotenv').config()
-//}
+if (process.env !== 'production'){
+require('dotenv').config()
+}
     
 const express = require ('express')
 const app = express()
